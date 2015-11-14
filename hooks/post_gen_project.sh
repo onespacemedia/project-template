000280948efd85c2af642393a15a137fcc450b94
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-14 16:06:59 +0000

    Fix templates folder name.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -30,3 +30,9 @@ pip freeze > requirements.txt
 
 # Generate a secret key and update the base settings file.
 sed -i '' "s|SECRET_KEY = \" \"|SECRET_KEY = \"$(printf '%q' $(./manage.py generate_secret_key))\"|g" {{cookiecutter.repo_name}}/settings/base.py
+
+# We disable rendering of the templates folder to avoid having to wrap everything in
+# raw tags, but that causes the parent directory path to also not be parse, so we need
+# to move everything into the correct location, then remove the old parent directory.
+mv {{ "{{" }}cookiecutter.repo_name{{ "}}" }}/templates {{cookiecutter.repo_name}}/templates
+rmdir {{ "{{" }}cookiecutter.repo_name{{ "}}" }}
