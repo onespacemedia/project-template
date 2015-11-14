7da501e75cccf91b5b2cdd061d2fe01f1b667115
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-14 13:34:51 +0000

    Add virtual environment logic to post_gen script. Resolves #4.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,13 +7,22 @@ npm install -g webpack
 npm install
 webpack
 
-# Install Python dependencies.
-pip install -r requirements.txt
+# Make the virtual environment.
+if which -s mkvirtualenv; then
+    mkvirtualenv {{cookiecutter.repo_name}}
+    workon {{cookiecutter.repo_name}}
+else
+    virtualenv -p python .venv
+    . .venv/bin/activate
+fi
 
 # If GeoIP wasn't enabled, delete the GeoIP folder.
 if grep -iq GeoIP "requirements.txt"; then
     rm -rf {{cookiecutter.repo_name}}/geoip/
 fi
 
-# The requirements will now have versions pinned.
+# Install Python dependencies.
+pip install -r requirements.txt
+
+# The requirements will now have versions pinned, so re-dump them.
 pip freeze > requirements.txt
