005d85c6d62f0d2b1954d0e05ab5af620560efb5
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-14 13:13:39 +0000

    Move `createdb` into post_gen, add Pip things.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+createdb {{cookiecutter.repo_name}}
+
+# Install front-end dependencies.
 npm install -g webpack
 npm install
 webpack
-pip freeze > requirements.txt
 
-# TODO: If geoip wasn't enabled, delete the geoip folder.
+# Install Python dependencies.
+pip install -r requirements.txt
+
+# If GeoIP wasn't enabled, delete the GeoIP folder.
+if grep -iq GeoIP "requirements.txt"; then
+    rm -rf {{cookiecutter.repo_name}}/geoip/
+fi
+
+# The requirements will now have versions pinned.
+pip freeze > requirements.txt
