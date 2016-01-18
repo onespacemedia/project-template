6631e6bfbf318af751f9db19bbb5fd34b5667edd
Dan Gamble <dangamble89@gmail.com>
2016-01-18 14:56:07 +0000

    Trimmed loads of postcss plugins away and refactored the mixins + variable handling for CSS

--- {{cookiecutter.repo_name}}/gulpfile.js/lib/postCssProcessors.js
+++ {{cookiecutter.repo_name}}/gulpfile.js/lib/postCssProcessors.js
@@ -1,48 +1,25 @@
+var path = require('path')
+
 module.exports = [
-  // Sassy based plugins
   require('postcss-import')({
     glob: true
   }),
-  require('postcss-sassy-mixins'),
-  require('postcss-advanced-variables'),
-  require('postcss-custom-selectors'),
-  require('postcss-custom-media'),
-  require('postcss-custom-properties'),
-  require('postcss-color-function'),
-  require('postcss-nested'),
-  require('postcss-atroot'),
-  require('postcss-property-lookup'),
-  require('postcss-selector-matches'),
-  require('postcss-selector-not'),
+  require('postcss-mixins'),
   require('postcss-functions')({
-    functions: {
-      percentage: function(val1, val2) {
-        var number = (val1 / val2) * 100
-
-        return number.toFixed(2) + '%'
-      }
-    }
+    glob: path.join(__dirname, '../../example_project', 'assets', 'css', 'functions', '*.js')
   }),
-  require('postcss-map')({
-    basePath: '{{cookiecutter.package_name}}/assets/css/config',
-    maps: ['breakpoints.yaml', 'colors.yaml', 'fonts.yaml', 'grid.yaml', 'misc.yaml']
-  }),
-  require('postcss-calc'),
 
   // Niceties
-  require('postcss-assets')({
-    basePath: '{{cookiecutter.package_name}}/assets/',
-    loadPaths: ['img/'],
-    baseUrl: '/static/'
-  }),
   require('postcss-brand-colors'),
-  require('postcss-fakeid'),
   require('postcss-property-lookup'),
   require('postcss-pxtorem'),
-  require('postcss-quantity-queries'),
   require('postcss-will-change'),
+  require('postcss-cssnext')({
+    features: {
+      rem: false
+    }
+  }),
   require('autoprefixer')({
     browsers: ['> 1%', 'IE 9', 'IE 10', 'last 2 versions']
   }),
 ];
-
