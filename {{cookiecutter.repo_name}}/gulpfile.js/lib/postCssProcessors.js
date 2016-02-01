b2f6896e65eadc2e3dea3ad078fad1c3dfafcd4e
Dan Gamble <dangamble89@gmail.com>
2016-02-01 13:20:22 +0000

    Updated the PostCSS processors and package.json

--- {{cookiecutter.repo_name}}/gulpfile.js/lib/postCssProcessors.js
+++ {{cookiecutter.repo_name}}/gulpfile.js/lib/postCssProcessors.js
@@ -4,21 +4,33 @@ module.exports = [
   require('postcss-import')({
     glob: true
   }),
-  require('postcss-mixins'),
+  require('postcss-sassy-mixins'),
+  require('postcss-conditionals'),
+  require('postcss-nested'),
+  require('postcss-simple-vars'),
   require('postcss-functions')({
     glob: path.join(__dirname, '../../{{cookiecutter.repo_name}}', 'assets', 'css', 'functions', '*.js')
   }),
 
   // Niceties
+  require('postcss-assets')({
+    basePath: 'streets/assets/',
+    loadPaths: ['img/'],
+    baseUrl: '/static/'
+  }),
   require('postcss-brand-colors'),
+  require('postcss-color-alpha'),
   require('postcss-property-lookup'),
   require('postcss-pxtorem'),
   require('postcss-will-change'),
   require('postcss-cssnext')({
     features: {
+      autoprefixer: false,
+      nesting: false,
       rem: false
     }
   }),
+  require('postcss-round-subpixels'),
   require('autoprefixer')({
     browsers: ['> 1%', 'IE 9', 'IE 10', 'last 2 versions']
   }),
