e9ca3297fb9d5fb162b6d3b7dd1f56faec7c9989
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-05 11:57:41 +0000

    Add final Docker files while the work is put on hold.

--- {{cookiecutter.repo_name}}/manage.sh
+++ {{cookiecutter.repo_name}}/manage.sh
@@ -4,19 +4,21 @@ if [ "$1" == "" ]; then
   echo "Create and manage Docker instances for this project."
   echo
   echo "Commands:"
-  echo -e "  configure\t\tCreate, start and deploy a new Docker instance."
+  echo -e "  configure\t\t\tCreate, start and deploy a new Docker instance."
+  echo -e "  <manage.py subcommand>\tRun a manage.py subcommand on the Docker instance (e.g. migrate, createsuperuser)."
   echo
 elif [ "$1" == "configure" ]; then
-  docker-machine create {{ cookiecutter.repo_name }} -d virtualbox
-  docker-machine start {{ cookiecutter.repo_name }}
-  eval "$(docker-machine env {{ cookiecutter.repo_name }})"
+  docker-machine create dev -d virtualbox
+  docker-machine start dev
+  eval "$(docker-machine env dev)"
+  docker stop $(docker ps -aq)
+  docker rm $(docker ps -aq)
   docker-compose -f dev.yml build
-  docker-compose -f dev.yml up -d
 
-  echo "Django server should now be running at http://$(docker-machine ip {{ cookiecutter.repo_name }}):8000/"
-  open "http://$(docker-machine ip {{ cookiecutter.repo_name }}):8000/"
+  echo "Django server should now be running at http://$(docker-machine ip dev):8000/"
+  docker-compose -f dev.yml up -d
 else
-  docker-machine start {{ cookiecutter.repo_name }}
-  eval "$(docker-machine env {{ cookiecutter.repo_name }})"
+  docker-machine start dev
+  eval "$(docker-machine env dev)"
   docker-compose -f dev.yml run django python manage.py "$@"
 fi
