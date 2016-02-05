d00c597f5b885799373996aa2b06c40b98da97c8
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-05 16:07:26 +0000

    Change `frontend_templates` reference.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/local.py
@@ -58,4 +58,10 @@ EMAIL_HOST_PASSWORD = '5033a6d5bca3f0'
 EMAIL_PORT = '2525'
 EMAIL_USE_TLS = True
 
-FRONTEND_TEMPLATES = lambda: [str(f[:-5]) for f in os.listdir(os.path.join(TEMPLATE_DIRS[0], 'frontend')) if f[:1] != '_']
+
+def frontend_templates():
+    return [
+        str(f[:-5])
+        for f in os.listdir(os.path.join(TEMPLATE_DIRS[0], 'frontend'))
+        if f[:1] != '_'
+    ]
