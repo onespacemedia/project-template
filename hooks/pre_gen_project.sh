ecdfee5c4a3ed86b06c97f3dc011dd0b26545601
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-15 16:52:49 +0000

    Change 'jobs' to 'careers' in pre_gen hook.

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -7,7 +7,7 @@ rm -rf tmp/
 # Pull in any optional project apps from their respective git repos.
 # They will have their contents rendered as part of the generation
 # process, then we'll move the files to their final destination.
-{% for project in ['jobs', 'faqs', 'people', 'news', 'sections'] %}
+{% for project in ['careers', 'faqs', 'people', 'news', 'sections'] %}
     {% if cookiecutter[project] == 'yes' %}
         echo "Cloning the {{project}} repo.";
         git clone --depth 1 -q https://github.com/onespacemedia/cms-{{project}}.git tmp/{{project}}/
