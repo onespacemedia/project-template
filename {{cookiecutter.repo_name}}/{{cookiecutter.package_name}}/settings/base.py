2fa57519c24e7d6ba2b4f5d049724f936cab6edc
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-18 16:20:55 +0000

    Remove `XFrameOptionsMiddleware` from the default settings
    This is covered by nginx

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
@@ -205,7 +205,6 @@ MIDDLEWARE_CLASSES = (
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "watson.middleware.SearchContextMiddleware",
     "historylinks.middleware.HistoryLinkFallbackMiddleware",
     "cms.middleware.PublicationMiddleware",
