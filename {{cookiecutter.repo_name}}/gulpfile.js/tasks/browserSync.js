f73e196fe1e46b02c999fb6541be901418218558
Lewis Collard <lewis.collard@onespacemedia.com>
2016-03-09 09:18:21 +0000

    Use cookiecutter.package_name rather than cookiecutter.project_name in browserSync.js. Fixes #32.

--- {{cookiecutter.repo_name}}/gulpfile.js/tasks/browserSync.js
+++ {{cookiecutter.repo_name}}/gulpfile.js/tasks/browserSync.js
@@ -5,7 +5,7 @@ var gulp        = require('gulp')
 var browserSyncTask = function() {
   browserSync.init(config.tasks.browserSync)
 
-  gulp.watch(['{{cookiecutter.project_name}}/apps/**/templates/**/*.html', '{{cookiecutter.project_name}}/templates/**/*.html'], browserSync.reload)
+  gulp.watch(['{{cookiecutter.package_name}}/apps/**/templates/**/*.html', '{{cookiecutter.package_name}}/templates/**/*.html'], browserSync.reload)
 }
 gulp.task('browserSync', browserSyncTask)
 module.exports = browserSyncTask
