c954ae7ee418bf8e6d342772e894b6b543eaabd6
Dan Gamble <dangamble89@gmail.com>
2016-04-06 15:33:01 +0100

    Small code style tweak

--- {{cookiecutter.repo_name}}/gulpfile.js/tasks/browserSync.js
+++ {{cookiecutter.repo_name}}/gulpfile.js/tasks/browserSync.js
@@ -5,7 +5,10 @@ var gulp        = require('gulp')
 var browserSyncTask = function() {
   browserSync.init(config.tasks.browserSync)
 
-  gulp.watch(['{{cookiecutter.package_name}}/apps/**/templates/**/*.html', '{{cookiecutter.package_name}}/templates/**/*.html'], browserSync.reload)
+  gulp.watch([
+    '{{cookiecutter.package_name}}/apps/**/templates/**/*.html',
+    '{{cookiecutter.package_name}}/templates/**/*.html'
+  ], browserSync.reload)
 }
 gulp.task('browserSync', browserSyncTask)
 module.exports = browserSyncTask
