1694e9b2db7790385536257c2a5790ac7103e763
Dan Gamble <dangamble89@gmail.com>
2016-09-19 08:58:14 +0100

    Update the references to _navigation_entries

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/templatetags/site.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/templatetags/site.py
@@ -1,11 +1,11 @@
 import json
 import jinja2
 
-from cms.apps.pages.templatetags.pages import navigation_entries
+from cms.apps.pages.templatetags.pages import _navigation_entries
 from django_jinja import library
 
 
 @library.global_function
 @jinja2.contextfunction
 def navigation_json(context, pages, section=None):
-    return json.dumps(navigation_entries(context, pages, section, is_json=True))
+    return json.dumps(_navigation_entries(context, pages, section, is_json=True))
