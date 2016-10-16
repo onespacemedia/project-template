1ae794168b6069bd1168023c54b90fdbf1a62597
Dan Gamble <dangamble89@gmail.com>
2016-10-16 13:07:44 +0100

    Update npm packages and upgrade webpack to 2.0

--- {{cookiecutter.repo_name}}/gulpfile.js/lib/webpack-multi-config.js
+++ {{cookiecutter.repo_name}}/gulpfile.js/lib/webpack-multi-config.js
@@ -1,82 +1,80 @@
 var config = require('../config')
 if(!config.tasks.js) return
 
-var path                          = require('path')
-var pathToUrl                     = require('./pathToUrl')
-var webpack                       = require('webpack')
-var webpackManifest               = require('./webpackManifest')
-var BundleTracker                 = require('webpack-bundle-tracker')
-var ExtractText                   = require('extract-text-webpack-plugin')
-var LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+var path            = require('path')
+var pathToUrl       = require('./pathToUrl')
+var webpack         = require('webpack')
+var webpackManifest = require('./webpackManifest')
+var BundleTracker   = require('webpack-bundle-tracker')
+var ExtractText     = require('extract-text-webpack-plugin')
 
 module.exports = function(env) {
   var jsSrc = path.resolve(config.root.src, config.tasks.js.src)
   var jsDest = path.resolve(config.root.dest, config.tasks.js.dest)
   var publicPath = pathToUrl('/static/build/', config.tasks.js.dest, '/')
 
-  var extensions = config.tasks.js.extensions.map(function(extension) {
-    return '.' + extension
-  })
-
   var filenamePattern = '[name].js'
 
   var webpackConfig = {
     context: jsSrc,
     plugins: [
-      new BundleTracker({filename: './webpack-stats.json'}),
-      new ExtractText('[name].css')
+      new BundleTracker({filename: './webpack-stats.json'})
     ],
     resolve: {
-      root: jsSrc,
-      extensions: [''].concat(extensions),
+      modules: [jsSrc, 'node_modules'],
+      extensions: ['.js', '.vue', '.css', '.json'],
       alias: {
-        'vue': 'vue/dist/vue.js'
+        'vue$': 'vue/dist/vue'
       }
     },
     module: {
-      preLoaders: [
+      rules: [
         {
+          enforce: 'pre',
           test: /\.vue$/,
           loader: 'eslint',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          },
+          include: jsSrc,
           exclude: /(node_modules|bower_components|vendor)/
         },
         {
+          enforce: 'pre',
           test: /\.js$/,
           loader: 'eslint',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          },
+          include: jsSrc,
           exclude: /(node_modules|bower_components|vendor)/
-        }
-      ],
-      loaders: [
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue',
+          options: {
+            postcss: require('../lib/postCssProcessors'),
+            loaders: {
+              js: 'babel'
+            }
+          }
+        },
         {
           test: /\.js$/,
           loader: 'babel',
-          exclude: /node_modules/,
-          query: config.tasks.js.babel
-        },
-        {
-          test: /\.vue$/,
-          loader: 'vue'
+          include: jsSrc,
+          exclude: /node_modules/
         },
         {
           test: /\.json$/,
           loader: 'json'
         }
       ]
-    },
-    vue: {
-      postcss: require('../lib/postCssProcessors'),
-      loaders: {
-        js: 'babel',
-        css: ExtractText.extract('css')
-      }
-    },
-    eslint: {
-      formatter: require('eslint-friendly-formatter')
     }
   }
 
   if(env === 'development') {
-    webpackConfig.devtool = 'inline-source-map'
+    webpackConfig.devtool = '#eval-source-map'
 
     // Create new entries object with webpack-hot-middleware added
     for (var key in config.tasks.js.entries) {
@@ -85,7 +83,16 @@ module.exports = function(env) {
       config.tasks.js.entries[key] = ['webpack-hot-middleware/client?&reload=true'].concat(entry)
     }
 
-    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
+    webpackConfig.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('development')
+        }
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    )
   }
 
   if(env !== 'test') {
@@ -111,17 +118,33 @@ module.exports = function(env) {
 
   if(env === 'production') {
     webpackConfig.plugins.push(
+      new ExtractText('[name][contenthash:8].css'),
       new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
       }),
-      new LodashModuleReplacementPlugin,
-      new webpack.optimize.OccurenceOrderPlugin,
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: false
+        }
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin,
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin(),
       new webpack.NoErrorsPlugin()
     )
+
+    webpackConfig.module.rules[2].options.loaders.css = ExtractText.extract({
+      loader: 'css',
+      fallBackLoader: 'vue-style-loader'
+    })
   }
 
   return webpackConfig
