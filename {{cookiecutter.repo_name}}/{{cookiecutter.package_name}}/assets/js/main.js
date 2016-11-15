f009d9bf67afea16f2ea67090dc51d45e77237d9
Daniel Samuels <daniel.samuels1@gmail.com>
2016-11-15 17:37:58 +0000

    Add external links library. Resolves #54. (#67)
    * Add external links library. Resolves #54.
    
    * Move utils folder to be a single file.
    
    * Change method of getting all links.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/main.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 
+import {externalLinks} from './utils'
+
 new Vue(App).$mount('#app')
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Stuff
+  externalLinks()
 })
