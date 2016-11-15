ff9ce4858f9be604dd0dfcae533cb1b2dc17e01d
Daniel Samuels <daniel@onespacemedia.com>
2016-11-15 16:54:23 +0000

    Add `CallToAction` model to a new components app. Resolves #56.

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/components/models.py
@@ -0,0 +1,39 @@
+from cms.apps.pages.models import Page
+from django.db import models
+
+
+class CallToAction(models.Model):
+
+    super_title = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    title = models.CharField(
+        max_length=140
+    )
+
+    link_page = models.ForeignKey(
+        Page,
+        blank=True,
+        null=True,
+        help_text='If you want to link to an internal page, please use this.'
+    )
+
+    link_url = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        help_text='If you want to link to an external page, please use this.'
+    )
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def link_location(self):
+        return self.link_page.get_absolute_url() if self.link_page else self.link_url
+
+    class Meta:
+        abstract = True
