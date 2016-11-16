0df528f713380d766a1f6184abb111a6eb5eae12
Lewis Collard <lewis.collard@onespacemedia.com>
2016-11-16 10:33:10 +0000

    Use single quotes consistently in settings.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/local.py
@@ -1,6 +1,6 @@
-"""
+'''
 Settings for local development.
-"""
+'''
 
 import os
 import os.path
@@ -11,37 +11,37 @@ from .base import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
 DEBUG = True
 
-TEMPLATES[0]["OPTIONS"]["auto_reload"] = DEBUG
+TEMPLATES[0]['OPTIONS']['auto_reload'] = DEBUG
 
 # Save media files to the user's Sites folder.
 
-MEDIA_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "media"))
-STATIC_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "static"))
-NODE_MODULES_ROOT = os.path.expanduser(os.path.join("~/Workspace/{{cookiecutter.package_name}}", "node_modules"))
+MEDIA_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'media'))
+STATIC_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'static'))
+NODE_MODULES_ROOT = os.path.expanduser(os.path.join('~/Workspace/{{cookiecutter.package_name}}', 'node_modules'))
 
 
 # Use local server.
 
-SITE_DOMAIN = "localhost:8000"
+SITE_DOMAIN = 'localhost:8000'
 
 ALLOWED_HOSTS = [
     # Django's defaults.
-    "127.0.0.1",
-    "localhost",
-    "::1",
+    '127.0.0.1',
+    'localhost',
+    '::1',
     # For compatibility with Browsersync.
-    "0.0.0.0",
+    '0.0.0.0',
 ]
 
 PREPEND_WWW = False
 
 # Optional separate database settings
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": "localhost",
-        "NAME": "{{cookiecutter.package_name}}",
-        "USER": os.getlogin(),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': '{{cookiecutter.package_name}}',
+        'USER': os.getlogin(),
     },
 }
 
