6330f8c33107130bea84e5fa5fb81e7830209edc
Daniel Samuels <daniel.samuels1@gmail.com>
2016-12-16 13:57:11 +0000

    Add `pre-push` requirement script.

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -3,7 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.package_name}}.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imperial.settings.local")
+
+    # Ensure we have a pre-push Git hook.
+    os.system("""
+        if [ ! -f .git/hooks/pre-push ]; then
+            wget -qO .git/hooks/pre-push https://raw.githubusercontent.com/onespacemedia/project-template/develop/%7B%7Bcookiecutter.repo_name%7D%7D/pre-push;
+            chmod +x .git/hooks/pre-push;
+        fi
+    """)
 
     from django.core.management import execute_from_command_line
 
