c870691d8420689ac7948ccd328d19ea58e01a4d
Daniel Samuels <daniel.samuels1@gmail.com>
2016-12-16 13:57:53 +0000

    Update manage.py

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imperial.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.package_name}}.settings.local")
 
     # Ensure we have a pre-push Git hook.
     os.system("""
