dd11a09ccd5746b7fe50964df8fb358f5c3fb157
Dan Gamble <dan@dangamble.co.uk>
2017-01-03 10:58:33 +0000

    Add safari iframe fix (#107)
    * Add safari iframe fix
    
    * Only apply iframe fix to Safari

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/main.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/main.js
@@ -3,10 +3,15 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './vue/App'
 
-import { externalLinks } from './utils'
+import { externalLinks, iframeFix } from './utils'
 
 new Vue(App).$mount('#app')
 
 document.addEventListener('DOMContentLoaded', () => {
   externalLinks()
+
+  // If the browser isn't Safari, don't do anything
+  if (document.querySelector('iframe') && window.navigator.userAgent.indexOf("Safari") > -1) {
+    iframeFix()
+  }
 })
