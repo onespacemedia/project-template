a4de690f63d491e7634d2da15230dd4422bb2d88
Daniel Samuels <daniel@onespacemedia.com>
2017-06-05 09:53:18 +0100

    Comment-out the CTA by default. Resolves #103.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/components/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/components/models.py
@@ -1,39 +1,36 @@
-from cms.apps.pages.models import Page
-from django.db import models
+# from cms.apps.pages.models import Page
+# from django.db import models
 
 
-class CallToAction(models.Model):
+# class CallToAction(models.Model):
 
-    super_title = models.CharField(
-        max_length=100,
-        blank=True,
-        null=True
-    )
+#     super_title = models.CharField(
+#         max_length=100,
+#         blank=True,
+#         null=True
+#     )
 
-    title = models.CharField(
-        max_length=140
-    )
+#     title = models.CharField(
+#         max_length=140
+#     )
 
-    link_page = models.ForeignKey(
-        Page,
-        blank=True,
-        null=True,
-        help_text='If you want to link to an internal page, please use this.'
-    )
+#     link_page = models.ForeignKey(
+#         Page,
+#         blank=True,
+#         null=True,
+#         help_text='If you want to link to an internal page, please use this.'
+#     )
 
-    link_url = models.CharField(
-        max_length=200,
-        blank=True,
-        null=True,
-        help_text='If you want to link to an external page, please use this.'
-    )
+#     link_url = models.CharField(
+#         max_length=200,
+#         blank=True,
+#         null=True,
+#         help_text='If you want to link to an external page, please use this.'
+#     )
 
-    def __unicode__(self):
-        return self.title
+#     def __unicode__(self):
+#         return self.title
 
-    @property
-    def link_location(self):
-        return self.link_page.get_absolute_url() if self.link_page else self.link_url
-
-    class Meta:
-        abstract = True
+#     @property
+#     def link_location(self):
+#         return self.link_page.get_absolute_url() if self.link_page else self.link_url
