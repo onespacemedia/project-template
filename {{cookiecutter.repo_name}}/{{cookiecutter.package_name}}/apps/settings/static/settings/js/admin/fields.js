69663f01b43596b8d68dcab3e6c69f1340979920
Dan Gamble <dangamble89@gmail.com>
2017-06-15 11:12:33 +0100

    [Ref #146] Add JS Prettier

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/settings/static/settings/js/admin/fields.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/settings/static/settings/js/admin/fields.js
@@ -1,42 +1,35 @@
-$(function() {
-
-    setInterval(hide_inline_section_fields, 100)
-});
+$(function () {
+  setInterval(hide_inline_section_fields, 100)
+})
 
 //	Function to hide fields of a section inline based on its type
-function hide_inline_section_fields() {
+function hide_inline_section_fields () {
+  //	Define which section types have which elements
+  var elements = {
+    '.field-string': ['string'],
+    '.field-text': ['text'],
+    '.field-number': ['number'],
+    '.field-image': ['image']
+  }
 
-    //	Define which section types have which elements
-    var elements = {
-        '.field-string': ['string'],
-        '.field-text': ['text'],
-        '.field-number': ['number'],
-        '.field-image': ['image'],
-    };
+  //	Loop all of the section inlines
+  $('.tab-content').each(function () {
+    var _this = this
 
-    //	Loop all of the section inlines
-    $('.tab-content').each(function() {
+    //	Get the type select of the section
+    var select = $('[id$=_type]', $(this))[0].value
+    select = select == '' ? '0' : select
 
-        var _this = this
+    //	Loop the elements
+    for (var element in elements) {
+      var element_object = $(element, $(this))
 
-        //	Get the type select of the section
-        var select = $('[id$=_type]', $(this))[0].value;
-        select = select == '' ? '0' : select;
-
-        //	Loop the elements
-        for (var element in elements) {
-
-            var element_object = $(element, $(this))
-
-            // Hide / show the element based on select index
-            if (elements[element].indexOf(select) != -1) {
-                element_object.css('display', '');
-            }
-            else {
-                element_object.css('display', 'none');
-            }
-        }
-
-    })
-
+      // Hide / show the element based on select index
+      if (elements[element].indexOf(select) != -1) {
+        element_object.css('display', '')
+      } else {
+        element_object.css('display', 'none')
+      }
+    }
+  })
 }
--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/utils.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/utils.js
@@ -1,10 +1,10 @@
 /*
-External links (defined as all links pointing to a domain that is not the
-same as the one from which the current document is being served)
-will have their 'target' attribute set to '_blank'.
+ External links (defined as all links pointing to a domain that is not the
+ same as the one from which the current document is being served)
+ will have their 'target' attribute set to '_blank'.
 
-Social media sharing links will show in a popup.
-*/
+ Social media sharing links will show in a popup.
+ */
 export function externalLinks () {
   const links = Array.from(document.getElementsByTagName('a'))
 
@@ -23,13 +23,17 @@ export function externalLinks () {
   }
 
   for (const link of links) {
-    const href = link.getAttribute('href').trim()
+    const href = link.getAttribute('href')
 
     if (!href) {
       continue
     }
 
-    if (href.indexOf('http://') === 0 || href.indexOf('https://') === 0 || href.indexOf('//') === 0) {
+    if (
+      href.indexOf('http://') === 0 ||
+      href.indexOf('https://') === 0 ||
+      href.indexOf('//') === 0
+    ) {
       let domain = link.hostname
 
       if (domain.indexOf('www.') === 0) {
@@ -54,9 +58,13 @@ export function externalLinks () {
     // Use a popup for social sharing links.
     for (const prefix of usePopupPrefixes) {
       if (href.indexOf(prefix) === 0) {
-        link.addEventListener('click', (event) => {
+        link.addEventListener('click', event => {
           event.preventDefault()
-          window.open(href, '_blank', 'width=600,height=300,menubar=0,toolbar=0,status=0')
+          window.open(
+            href,
+            '_blank',
+            'width=600,height=300,menubar=0,toolbar=0,status=0'
+          )
         })
 
         break
@@ -69,10 +77,10 @@ export function externalLinks () {
  Media breakpoints
  */
 export const mediaBreakpoints = {
-  'sm': 600,
-  'md': 900,
-  'lg': 1200,
-  'xlg': 1800
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xlg: 1800
 }
 
 /*
@@ -92,5 +100,110 @@ export function iframeFix () {
       parent.insertBefore(iframeCopy, iframe)
       parent.removeChild(iframe)
     }
+  }
+}
+
+export function getOffsetTop (el, parent = document.body) {
+  let offsetTop = 0
+
+  do {
+    if (!isNaN(el.offsetTop)) {
+      offsetTop += el.offsetTop
+    }
+
+    el = el.offsetParent
+  } while (el !== parent || el !== document.body)
+
+  return offsetTop
+}
+
+export function getOffsetLeft (el, parent = document.body) {
+  let offsetLeft = 0
+
+  do {
+    if (!isNaN(el.offsetLeft)) {
+      offsetLeft += el.offsetLeft
+    }
+
+    el = el.offsetParent
+  } while (el !== parent || el !== document.body)
+
+  return offsetLeft
+}
+
+export function scrollToY (
+  scrollTargetY = 0,
+  speed = 300,
+  easing = 'easeInOutQuint'
+) {
+  const scrollY = window.scrollY
+  let currentTime = 0
+
+  const time = Math.max(
+    0.1,
+    Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8)
+  )
+
+  const easingEquations = {
+    easeOutSine (pos) {
+      return Math.sin(pos * (Math.PI / 2))
+    },
+    easeInOutSine (pos) {
+      return -0.5 * (Math.cos(Math.PI * pos) - 1)
+    },
+    easeInOutQuint (pos) {
+      if ((pos /= 0.5) < 1) {
+        return 0.5 * Math.pow(pos, 5)
+      }
+      return 0.5 * (Math.pow(pos - 2, 5) + 2)
+    }
+  }
+
+  function tick () {
+    currentTime += 1 / 60
+
+    const position = currentTime / time
+    const t = easingEquations[easing](position)
+
+    if (position < 1) {
+      window.requestAnimationFrame(tick)
+
+      window.scrollTo(0, scrollY + (scrollTargetY - scrollY) * t)
+    } else {
+      window.scrollTo(0, scrollTargetY)
+    }
+  }
+
+  tick()
+}
+
+export class Cycler {
+  constructor (items) {
+    this.items = items
+    this.currentPosition = 0
+  }
+
+  current () {
+    return this.items[this.currentPosition]
+  }
+
+  set (val) {
+    this.currentPosition = val
+  }
+
+  prev () {
+    const current = this.current()
+    this.currentPosition = this.currentPosition - 1
+
+    if (this.currentPosition < 0) this.currentPosition = this.items.length - 1
+
+    return current
+  }
+
+  next () {
+    const current = this.current()
+    this.currentPosition = (this.currentPosition + 1) % this.items.length
+
+    return current
   }
 }
