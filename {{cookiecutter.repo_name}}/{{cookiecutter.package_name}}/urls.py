bbfe486d1f6942250211968df1365dd0e1ef9582
Dan Gamble <dan@dangamble.co.uk>
2017-06-20 11:44:54 +0100

    Update urls.py to be 1.11 compatible (#155)
    * Update urls.py to be 1.11 compatible
    
    * Add static media urls back

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.contenttypes import views as contenttypes_views
+from django.contrib.sitemaps import views as sitemaps_views
 from django.views import generic
 
 {% if cookiecutter.sections == 'no' %}# {% endif %}from .apps.sections.models import sections_js
@@ -16,19 +19,19 @@ admin.autodiscover()
 urlpatterns = [
 
     # Admin URLs.
-    url(r'^admin/password_change/$', 'django.contrib.auth.views.password_change',
+    url(r'^admin/password_change/$', auth_views.password_change,
         {'password_change_form': CMSPasswordChangeForm}, name='password_change'),
-    url(r'^admin/password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    url(r'^admin/password_change/done/$', auth_views.password_change_done, name='password_change_done'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', include('social_django.urls', namespace='social')),
     {% if cookiecutter.sections == 'no' %}# {% endif %}url(r'^admin/pages/page/sections.js$', sections_js, name='admin_sections_js'),
 
     # Permalink redirection service.
-    url(r'^r/(?P<content_type_id>\d+)-(?P<object_id>[^/]+)/$', 'django.contrib.contenttypes.views.shortcut', name='permalink_redirect'),
+    url(r'^r/(?P<content_type_id>\d+)-(?P<object_id>[^/]+)/$', contenttypes_views.shortcut, name='permalink_redirect'),
 
     # Google sitemap service.
-    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': registered_sitemaps}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': registered_sitemaps}),
+    url(r'^sitemap.xml$', sitemaps_views.index, {'sitemaps': registered_sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', sitemaps_views.sitemap, {'sitemaps': registered_sitemaps}),
 
     # Basic robots.txt.
     url(r'^robots.txt$', TextTemplateView.as_view(template_name='robots.txt')),
@@ -37,8 +40,6 @@ urlpatterns = [
     url(r'^favicon.ico$', generic.RedirectView.as_view(permanent=True)),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-) + static(
-    settings.NODE_MODULES_URL, document_root=settings.NODE_MODULES_ROOT
 )
 
 
