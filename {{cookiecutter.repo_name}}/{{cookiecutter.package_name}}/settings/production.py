35089e31d6ab7ae6c99ca91295c7690ff3a73990
Dan Gamble <dan@dangamble.co.uk>
2017-09-11 13:09:40 +0100

    Remove CSRF_COOKIE_HTTPONLY and move CSRF_COOKIE_SECURE to production.py (#172)
    As per
    https://docs.djangoproject.com/en/1.11/ref/settings/#csrf-cookie-httponly
    it mentions this isn't really needed and prevents our AJAX requests from
    being able to read the CSRF

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/production.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/production.py
@@ -2,6 +2,7 @@ from .base import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+CSRF_COOKIE_SECURE = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
