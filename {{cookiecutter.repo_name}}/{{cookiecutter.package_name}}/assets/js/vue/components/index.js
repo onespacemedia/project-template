12bcdec86fdb4212e6c123cd3dbf874930bd6c5c
Lewis Collard <lewis.collard@onespacemedia.com>
2017-09-26 10:05:48 +0100

    [#180] Remove reference to no-longer-extant Vue component.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/vue/components/index.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/vue/components/index.js
@@ -1,9 +1,7 @@
 import FrontendSwitcher from './frontend-switcher/FrontendSwitcher.vue'
-import LazyImage from './images/LazyImage.vue'
 import MobileNav from './mobile-nav/MobileNav.vue'
 
 export default {
   FrontendSwitcher,
-  LazyImage,
   MobileNav
 }
