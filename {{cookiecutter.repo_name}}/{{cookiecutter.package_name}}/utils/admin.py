b64dddccde789e582bf1ea3a6ffe7df24613a042
Lewis Collard <lewis.collard@onespacemedia.com>
2017-10-19 12:11:46 +0100

    Document HasImageAdminMixin.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils/admin.py
@@ -86,6 +86,25 @@ class UsedOnAdminMixin(object):
 
 
 class HasImageAdminMixin(object):
+    '''
+    A helper to add a thumbnail into admin list views, for models that have an
+    ImageRefField field.
+
+    Inherit from this and add `get_image` to your `list_display` in the place
+    you want to show the image. If you have the image set as the first column
+    in the list, you'll probably want to override `list_display_links` as
+    well.
+
+    class MyModelAdmin(HasImageAdminMixin, admin.ModelAdmin):
+        list_display = ['get_image', '__str__']
+        list_display_links = ['get_image', '__str__']
+
+    It assumes that the field is called "image"; if it's called something else
+    on your model, override the `image_field` attribute, e.g.:
+
+    class MyModelAdmin(HasImageAdminMixin, admin.ModelAdmin):
+        image_field = 'photo'
+    '''
     image_field = 'image'
 
     def get_queryset(self, request):
