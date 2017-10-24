aac3c2160f5f16b01f917e55751234fd3ca2ce2e
Lewis Collard <lewiscollard@gmail.com>
2017-10-24 11:33:10 +0100

    [#99] Make Argon2 the preferred password hasher. (#200)
    * [#99] Make Argon2 the preferred password hasher.
    
    * Remove all the other password hashers that are not Argon2.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
@@ -209,13 +209,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
 )
 
 
