c5166e63f728049af422d5560e10a6bd5e933b22
Lewis Collard <lewiscollard@gmail.com>
2017-10-24 11:36:20 +0100

    Change the way django_social models are unregistered. (#201)

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/__init__.py
@@ -1,2 +1,1 @@
 """Site-specific content, templatetags and such."""
-default_app_config = '{{cookiecutter.package_name}}.apps.site.apps.SiteAppConfig'
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from social_django.models import Association, Nonce, UserSocialAuth
+
+
+# Unregister python social auth models (not really needed or useful).
+for model in [Association, Nonce, UserSocialAuth]:
+    try:
+        admin.site.unregister(model)
+    except admin.sites.NotRegistered:
+        pass
