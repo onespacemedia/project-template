741c3ea89e7f7e7a8af9c7d35eecd726960c9755
Daniel Samuels <daniel@onespacemedia.com>
2017-10-25 11:58:56 +0100

    Fix DB issues on project creation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,14 +1,56 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -eo pipefail
 
-if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+function cleanup {
+    echo 'Removing project folder.'
+    rm -r "{{cookiecutter.package_name}}"
+    rm -r "{{ "{{" }}cookiecutter.package_name{{ "}}" }}"
+}
+
+trap cleanup ERR
+trap cleanup EXIT
+trap cleanup INT
+
+if [ -f ~/.zshrc ]; then
     source ~/.zshrc
 else
     source ~/.bash_profile
 fi
 
+# Check if the database already exists
+DB_NAME="{{cookiecutter.package_name}}"
+if psql {{cookiecutter.package_name}} -c ''; then
+    NEW_DB_NAME="${DB_NAME}_$(date +"%Y%m")"
+    echo "You are trying to create a new database '$DB_NAME' which already exists; we can't do that (the database name needs to be available)."
+    echo 'Please select a fix:'
+    echo " 1) Use an automatically generated name ($NEW_DB_NAME)"
+    echo ' 2) Provide a new name now'
+    echo ' 3) Drop the existing database'
+    echo ' 4) Quit, and let me sort it out'
+    echo 'Please note: The replacement database name is not validated, please ensure it is valid and available.'
+    read -p 'Select an option [1]: '
+
+    case "$REPLY" in
+        "1")
+            DB_NAME=$NEW_DB_NAME
+            ;;
+        "2")
+            read -p 'Please enter the database name you would like to use: ' DB_NAME
+            ;;
+        "3")
+            dropdb $DB_NAME
+            ;;
+        "4")
+            exit 1
+            ;;
+        *)
+            DB_NAME=$NEW_DB_NAME
+            ;;
+    esac
+fi
+
 # Create the database.
-createdb {{cookiecutter.package_name}}
+createdb $DB_NAME
 
 # Make the virtual environment.
 if command -v mkvirtualenv >/dev/null 2>&1; then
@@ -34,6 +76,8 @@ if [ -z "$CI" ]; then
     pip install --upgrade pip
 fi
 
+# Upgrade setuptools to avoid issues with python-memcached
+pip install --upgrade setuptools
 pip install -r requirements.txt
 
 # Install the linters so the versions get frozen.
@@ -59,7 +103,9 @@ perl -pi -e s,SECRET_KEY\ =\ \'\ \',SECRET_KEY\ =\ \'$(openssl rand -base64 50 |
         echo "Remove the {{project}} app.";
         rm -r {{ cookiecutter.package_name }}/apps/{{ project }}
     {% else %}
-        mv {{ "{{" }}cookiecutter.package_name{{ "}}" }}/apps/{{ project }}/templates {{cookiecutter.package_name}}/apps/{{ project }}/templates
+        if [ -d "{{ "{{" }}cookiecutter.package_name{{ "}}" }}/apps/{{ project }}/templates" ]; then
+            mv {{ "{{" }}cookiecutter.package_name{{ "}}" }}/apps/{{ project }}/templates {{cookiecutter.package_name}}/apps/{{ project }}/templates
+        fi
     {% endif %}
 {% endfor %}
 
