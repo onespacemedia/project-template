468a7d7f7d6bbb58bb8f0d747e19353d46fb51e6
Lewis Collard <lewiscollard@gmail.com>
2018-02-13 14:37:21 +0000

    Add current commit hash to CSS.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
@@ -408,6 +408,13 @@ WYSIWYG_OPTIONS = {
     'content_css': '/static/build/css/wysiwyg.css'
 }
 
+# Current commit hash, used for cache-busting CSS.
+try:
+    GIT_COMMIT_HASH = os.popen('git rev-parse --short HEAD').read().strip()
+# Catch everything so we don't stop the application starting if there's a problem.
+except:  # pylint: disable=bare-except
+    GIT_COMMIT_HASH = ''
+
 NEWS_APPROVAL_SYSTEM = False
 
 GOOGLE_ANALYTICS = '{{cookiecutter.google_analytics}}'
