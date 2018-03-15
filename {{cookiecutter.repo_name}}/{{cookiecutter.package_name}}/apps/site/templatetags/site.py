aa3cc4eab2769137a60c9a314a1fef01a5aa39c2
Daniel Clayton <daniel.cltn@gmail.com>
2018-03-15 14:09:41 +0000

    Use the pip version of lazy-image

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/templatetags/site.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/site/templatetags/site.py
@@ -12,7 +12,6 @@ from django.template.defaultfilters import stringfilter
 from django.urls import NoReverseMatch, reverse
 from django.utils.safestring import mark_safe
 from django_jinja import library
-from sorl.thumbnail import get_thumbnail
 
 from ..models import Footer, Header
 
@@ -47,53 +46,6 @@ def get_previous_by_field(obj, field):
     except Exception:  # pylint:disable=broad-except
         pass  # Will cause 'None' to be returned.
 
-
-@library.global_function
-def lazy_image(image, height=None, width=None, blur=True, max_width=1920, crop=None):  # pylint: disable=too-many-arguments
-    # Ideally we will use the images uploaded sizes to get our aspect ratio but in certain circumstances, like cards,
-    # we will use our own provided ones
-    if not height:
-        height = image.height
-
-    if not width:
-        width = image.width
-
-    aspect_ratio = height / width
-
-    if width > max_width:
-        width = max_width
-
-    # The aspect ratio will be used to size the image with a padding-bottom based element
-    aspect_ratio_percentage = '{}%'.format(aspect_ratio * 100)
-    small_image_url = get_thumbnail(image.file, str(int(width / 20))).url
-    large_image_url = get_thumbnail(image.file, f'{width}x{height}', crop=crop).url
-    large_image_url_2x = get_thumbnail(image.file, f'{width * 2}x{height * 2}', crop=crop).url
-
-    return {
-        'alt_text': image.alt_text or '',
-        'aspect_ratio': aspect_ratio_percentage,
-        'small_image_url': small_image_url,
-        'large_image_url': large_image_url,
-        'large_image_url_2x': large_image_url_2x,
-        'blur': blur
-    }
-
-
-@library.global_function
-@library.render_with('images/lazy.html')
-def render_lazy_image(image, height=None, width=None, blur=True, max_width=1920, crop=None):  # pylint: disable=too-many-arguments
-    """
-        Usage: {% raw %}{{ lazy_image(path.to.image) }}{% endraw %}
-        :param crop:
-        :param max_width:
-        :param blur:
-        :param image:
-        :param height:
-        :param width
-        :return:
-    """
-
-    return lazy_image(image, height, width, blur, max_width, crop)
 
 
 @library.global_function
