1f55e7455c52349437e561949c5cf0b2fdb32c40
daniel-clayton <daniel.cltn@gmail.com>
2018-10-08 16:43:28 +0100

    Add contact app [WIP] (#271)
    * Add contact app [WIP]
    
    * Include django-recaptcha in requirements, fix lint/iSort
    
    * Don't fix what isn't broken
    
    * Add a content type icon.
    
    * Various text changes.
    
    * Add email filters and unit tests.
    
    * Rename get_email_addresses to email_addresses
    
    * Move clean to the model
    
    * Delete contact app if not selected to be installed.
    
    * Use built in url_from_path
    
    * Fix iSort error
    
    * Add utils.utils from develop branch.
    
    * On second thoughts, that shouldn't have been in the contact PR (blame me).
    
    * Change verbose_names to be lower case and user-visible strings to be punctuated sentences.
    
    * Make email_addresses a cached_property, simplify how email addresses are split.

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/contact/views.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.views.generic import CreateView, TemplateView
+
+from ..contact.models import ContactSubmission
+from .forms import ContactForm
+
+
+class ContactView(CreateView):
+    model = ContactSubmission
+
+    form_class = ContactForm
+
+    template_name = 'contact/contact.html'
+
+    def form_valid(self, form):
+        context = {
+            'first_name': form.cleaned_data['first_name'],
+            'last_name': form.cleaned_data['last_name'],
+            'phone_number': form.cleaned_data['phone_number'],
+            'reason_for_enquiry': form.cleaned_data['reason_for_enquiry'],
+            'email': form.cleaned_data['email'],
+            'job_title': form.cleaned_data['job_title'],
+            'message': form.cleaned_data['message'],
+            'settings': settings,
+        }
+
+        email_text_template = render_to_string('emails/contact-form.txt', context)
+
+        email_html_template = render_to_string('emails/contact-form.html', context)
+
+        email = EmailMultiAlternatives(
+            subject=f'New contact form submission on {settings.SITE_NAME}',
+            body=email_text_template,
+            reply_to=[form.cleaned_data['email']],
+            to=self.request.pages.current.content.email_addresses,
+        )
+
+        email.attach_alternative(email_html_template, 'text/html')
+
+
+        try:
+            email.send()
+        except:  # pylint:disable=bare-except
+            if settings.DEBUG:
+                raise
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.request.pages.current.reverse('contact_success')
+
+
+class ContactSuccessView(TemplateView):
+    template_name = 'contact/success.html'
