6a0c1a1e4a1b5306eaab638662f9093051d0273a
Aidan Currah <aidancurrah@gmail.com>
2018-12-13 16:21:26 +0000

    Update compact inline

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/sections/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/sections/admin.py
@@ -1,4 +1,5 @@
 from cms.apps.pages.admin import page_admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse_lazy
 from jet.admin import CompactInline
 
@@ -11,11 +12,18 @@ class ContentSectionInline(CompactInline):
     filter_horizontal = ['people']
     fk_name = 'page'
 
-    class Media(object):
-        js = [reverse_lazy('admin_sections_js')]
+    class Media:
+        js = [
+            reverse_lazy('admin_sections_js'),
+            staticfiles_storage.url('cms/js/sortable/sortables.min.js'),
+            staticfiles_storage.url('admin/edit_inline/compact_inline.js'),
+        ]
 
         css = {
-            'all': ['/static/css/admin-sections.css'],
+            'all': [
+                staticfiles_storage.url('css/admin-sections.css'),
+                staticfiles_storage.url('admin/edit_inline/compact_inline.css'),
+            ],
         }
 
 
