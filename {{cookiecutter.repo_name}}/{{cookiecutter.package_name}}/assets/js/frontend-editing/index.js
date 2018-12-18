ae4c1ac2c6a5dea0006f3e6b1b9d3f2e2d3d5dcd
Aidan Currah <aidancurrah@gmail.com>
2018-12-18 14:57:22 +0000

    compare the iframe message origin to the current window's

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/frontend-editing/index.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/frontend-editing/index.js
@@ -17,44 +17,47 @@ export function setupEditing () {
   const images = Array.from(document.querySelectorAll('.js-ImageEditable')).map(el => new ImageEditable({ el }))
 
   window.addEventListener('message', (e) => {
-    const ajaxOptions = {
-      url: `/lazy-image-render/?pk=${e.data}`,
-      method: 'get',
-      xsrfCookieName: 'csrftoken',
-      xsrfHeaderName: 'X-CSRFToken',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
+    if (e.origin === window.location.origin) {
+      const ajaxOptions = {
+        url: `/ch/lazy-image-render/?pk=${e.data}`,
+        method: 'get',
+        xsrfCookieName: 'csrftoken',
+        xsrfHeaderName: 'X-CSRFToken',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
       }
-    }
 
-    axios(ajaxOptions)
-      .then(response => {
-        const tempDiv = document.createElement('div')
-        tempDiv.innerHTML = response.data.image_html
-        const image = tempDiv.firstChild
-        const originalImage = e.source.frameElement.previousElementSibling
-        originalImage.replaceChild(image, originalImage.firstChild)
-        new LazyImage({el: image})
-        e.source.frameElement.parentNode.removeChild(e.source.frameElement)
-        image.parentNode.dataset.value = e.data
+      axios(ajaxOptions)
+        .then(response => {
+          const tempDiv = document.createElement('div')
+          tempDiv.innerHTML = response.data.image_html
+          const image = tempDiv.firstChild
+          const originalImage = e.source.frameElement.previousElementSibling
+          originalImage.replaceChild(image, originalImage.firstChild)
+          new LazyImage({el: image})
+          e.source.frameElement.parentNode.removeChild(e.source.frameElement)
+          image.parentNode.dataset.value = e.data
 
-        for (const imageEditable of images) {
-          if (image.parentNode === imageEditable.el) {
-            imageEditable.sendData()
+          for (const imageEditable of images) {
+            if (image.parentNode === imageEditable.el) {
+              imageEditable.sendData()
+            }
           }
-        }
-      })
-      .catch(e => {
-        const { response } = e
-        const { status, data } = response
+        })
+        .catch(e => {
+          console.log(e)
+          const {response} = e
+          const {status, data} = response
 
-        // form_invalid return
-        if (status === 400) {
-          return
-        }
-        /* eslint-disable no-console */
-        console.error(e)
-        /* eslint-enable no-console */
-      })
+          // form_invalid return
+          if (status === 400) {
+            return
+          }
+          /* eslint-disable no-console */
+          console.error(e)
+          /* eslint-enable no-console */
+        })
+    }
   }, false)
 }
