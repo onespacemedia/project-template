0d785f01c19fd4ff7080bdea9964faa5288bbcc0
Daniel Clayton <daniel.cltn@gmail.com>
2019-03-06 16:19:57 +0000

    Include emails app in installed apps

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/base.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     {% if cookiecutter.careers == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.careers',
     '{{cookiecutter.package_name}}.apps.components',
     {% if cookiecutter.contact == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.contact',
+    {% if cookiecutter.emails == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.emails',
     {% if cookiecutter.events == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.events',
     {% if cookiecutter.faqs == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.faqs',
     {% if cookiecutter.news == 'no' %}# {% endif %}'{{cookiecutter.package_name}}.apps.news',
@@ -97,6 +98,7 @@ INSTALLED_APPS = [
     'cachalot',
     'webpack_loader',
 ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django_jinja.backend.Jinja2',
