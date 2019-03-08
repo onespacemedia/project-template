260205e0a1e61adffb1d0a6a90c840572c4d3851
Daniel Clayton <daniel.cltn@gmail.com>
2019-03-08 10:18:34 +0000

    Add resources app

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/resources/apps.py
@@ -0,0 +1,24 @@
+from django.apps import AppConfig
+from watson import search as watson
+
+
+class ResourcesConfig(AppConfig):
+    name = '{{ cookiecutter.package_name }}.apps.resources'
+
+    def ready(self):
+        # These imports and class definitions must be here in order to avoid
+        # an 'Apps are not ready yet exception' (as sitemaps imports from
+        # cms.models).
+
+        from cms.models import PageBaseSearchAdapter
+        from cms import sitemaps
+        Resource = self.get_model('Resource')
+
+        class ResourceSiteMap(sitemaps.PageBaseSitemap):
+            model = Resource
+
+            def items(self):
+                return super().items().filter(content__is_null=False)
+
+        watson.register(Resource, adapter_cls=PageBaseSearchAdapter)
+        sitemaps.register(Resource, sitemap_cls=ResourceSiteMap)
