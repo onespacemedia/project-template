ea4b858239d02e62e9253a49f18f68d0bb68bd74
Lewis Collard <lewis.collard@onespacemedia.com>
2019-05-03 16:53:42 +0100

    Add missing wrapElement helper JS. (#311)
    * Add missing wrapElement helper JS.
    
    * Elements are `elements`; `elms` are trees.

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/utils/index.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/assets/js/utils/index.js
@@ -143,3 +143,39 @@ export function removePreloadClass () {
     body.classList.remove('util-Preload')
   }, 500)
 }
+
+// Wrap an HTMLElement around either a single element, or each element in an
+// HTMLElement array.
+export function wrapElement (toWrap, elements) {
+  // Convert `elements` to an array, if necessary.
+  if (!elements.length) {
+    elements = [elements]
+  } else {
+    /* Array.from allows it to be used on an HTMLCollection. */
+    elements = Array.from(elements)
+  }
+
+  // Loops backwards to prevent having to clone the wrapper on the
+  // first element (see `child` below).
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const child = i > 0 ? toWrap.cloneNode(true) : toWrap
+    const el = elements[i]
+
+    // Cache the current parent and sibling.
+    const parent = el.parentNode
+    const sibling = el.nextSibling
+
+    // Wrap the element (is automatically removed from its current
+    // parent).
+    child.appendChild(el)
+
+    // If the element had a sibling, insert the wrapper before
+    // the sibling to maintain the HTML structure; otherwise, just
+    // append it to the parent.
+    if (sibling) {
+      parent.insertBefore(child, sibling)
+    } else {
+      parent.appendChild(child)
+    }
+  }
+}
