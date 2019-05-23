2ea6f0f4564012a6c7789b3d21e0b2168aeef092
Lewis Collard <lewis.collard@onespacemedia.com>
2019-05-23 10:35:23 +0100

    Remove pre-push downloading.

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -8,22 +8,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DB_USER", pwd.getpwuid(os.getuid()).pw_name)
     os.environ.setdefault("DB_NAME", "{{cookiecutter.package_name}}")
 
-    # Ensure we have a pre-push Git hook.
-    if not os.getenv('CI', False):
-        os.system("""
-            cd "$(git rev-parse --show-toplevel)" || exit;
-
-            if [ ! -f .git/hooks/pre-push ]; then
-                echo "Downloading the pre-push hook..";
-                curl -so .git/hooks/pre-push https://raw.githubusercontent.com/onespacemedia/project-template/develop/%7B%7Bcookiecutter.repo_name%7D%7D/pre-push;
-
-                echo "Installing the pre-push hook..";
-                chmod +x .git/hooks/pre-push;
-
-                echo "Done!";
-            fi
-        """)
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
