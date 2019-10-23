255ccbd14b05192d86c5ff3b8cbfdbc3ea34c3e6
Daniel Clayton <daniel.cltn@gmail.com>
2019-10-23 15:58:10 +0100

    Add the users app. Extends from the base django model but uses email_address as the username field.

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils/forms.py
@@ -0,0 +1,30 @@
+from django.core.exceptions import ValidationError
+
+from ..apps.users.models import User
+
+
+class UserEmailUniqueFormMixin(object):
+    '''
+    A mixin for ModelForm derivatives for the User model that want to
+    enforce no two users having the same *case-insensitive* email address
+    (case-sensitive uniqueness is already enforced). This includes both the
+    cases where a user is created, and when a user is changed - it'll allow
+    developers@onespacemedia.com to be changed to DEVELOPERS@ONESPACEMEDIA.COM
+    but won't allow anyone else to change it to the same.
+
+    This is currently in use in UserCreationForm and
+    UserCreationAdminForm in admin.py, but should probably be used for the
+    user-facing registration forms above too at some point.
+    '''
+    def clean_email(self):
+        value = self.cleaned_data.get('email')
+        if value:
+            users = User.objects.filter(email__iexact=value)
+
+            if self.instance and self.instance.pk:
+                users = users.exclude(pk=self.instance.pk)
+
+            if users.exists():
+                raise ValidationError('A user with that email address already exists.')
+
+        return value
