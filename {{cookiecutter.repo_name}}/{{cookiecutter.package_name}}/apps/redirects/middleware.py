e868f8b35dbc643e6879dcf76d3e3aec41bf0429
Daniel Clayton <daniel.cltn@gmail.com>
2020-10-13 12:13:31 +0100

    Update redirects middleware (drop MiddlewareMixin)

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/redirects/middleware.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/apps/redirects/middleware.py
@@ -2,28 +2,18 @@ import re
 
 from django import http
 from django.conf import settings
-from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import redirect
 
 from .models import Redirect
 
 
-class RedirectFallbackMiddleware(MiddlewareMixin):
-    def _redirect_for_path(self, path):
-        if not getattr(settings, "REDIRECTS_ENABLE_REGEX", False):
-            try:
-                return Redirect.objects.get(old_path=path)
-            except Redirect.DoesNotExist:
-                return None
+class RedirectFallbackMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-        for redirect in Redirect.objects.all():
-            if redirect.old_path == path:
-                return redirect
-            if redirect.regular_expression and re.match(redirect.old_path, path):
-                return redirect
+    def __call__(self, request):
+        response = self.get_response(request)
 
-        return None
-
-    def process_response(self, request, response):
         # No need to check for a redirect for non-404 responses.
         if response.status_code != 404:
             return response
@@ -41,7 +31,19 @@ class RedirectFallbackMiddleware(MiddlewareMixin):
         if r is not None:
             if r.new_path == '':
                 return http.HttpResponseGone()
-            return http.HttpResponsePermanentRedirect(r.sub_path(path)) if r.type == '301' else http.HttpResponseRedirect(r.sub_path(path))
+            return redirect(r.sub_path(path), permanent=r.type == '301')
 
-        # No redirect was found. Return the response.
         return response
+
+    def _redirect_for_path(self, path):
+        try:
+            return Redirect.objects.get(old_path=path)
+        except Redirect.DoesNotExist:
+            pass
+
+        if getattr(settings, "REDIRECTS_ENABLE_REGEX", False):
+            for obj in Redirect.objects.all():
+                if obj.regular_expression and re.match(obj.old_path, path):
+                    return redirect
+
+        return None
